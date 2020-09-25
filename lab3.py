091e07b5ef735816b8b221c5ac88bad8c481a740
Andrez Alcazar <ama2322@nau.edu>
2020-09-25 09:26:06 -0700

    I added one comment line

--- lab3.py
+++ lab3.py
@@ -1,3 +1,4 @@
+#I am just adding a comment for the purpose of a commit. 
 def main(): 
     message = input(" What would you like to do? \n      (1) encrypt \n      (2) decrypt\n")
     key = input(" Enter the key: ") 
