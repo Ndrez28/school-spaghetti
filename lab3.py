f218d63219312169b5ccf7d46c1d6c088dd3ecba
Ndrez28 <71838628+Ndrez28@users.noreply.github.com>
2020-09-25 08:35:10 -0700

    Add files via upload

---
+++ lab3.py
@@ -0,0 +1,9 @@
+def main(): 
+    message = input(" What would you like to do? \n      (1) encrypt \n      (2) decrypt\n")
+    key = input(" Enter the key: ") 
+    keyLength = len(key) 
+    
+    for key in range(keyLength): 
+        print(key % 6,sep = " ") 
+
+main() 
